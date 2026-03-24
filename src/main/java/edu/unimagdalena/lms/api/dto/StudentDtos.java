8d7822a357174b7c70bae5c4dcda9d230c56cb91
MirianMarriaga <mmmarriaga@unimagdalena.edu.co>
2026-03-24 18:43:44 -0500

    feat: Agregar campos a StudentDtos

--- src/main/java/edu/unimagdalena/lms/api/dto/StudentDtos.java
+++ src/main/java/edu/unimagdalena/lms/api/dto/StudentDtos.java
@@ -1,4 +1,23 @@
 package edu.unimagdalena.lms.api.dto;
+import java.io.Serializable;
+import java.time.Instant;
 
 public class StudentDtos {
+    public record StudentCreateRequest(
+            String email,
+            String fullName
+    ) implements Serializable {}
+
+    public record StudentUpdateRequest(
+            String email,
+            String fullName
+    ) implements Serializable {}
+
+    public record StudentResponse(
+            long id,
+            String email,
+            String fullName,
+            Instant createdAt,
+            Instant updatedAt
+    ) implements Serializable {}
 }
