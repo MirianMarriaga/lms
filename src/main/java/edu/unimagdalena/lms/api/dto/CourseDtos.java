a4205ed6527728230c8ed420c6f7073458586f2d
MirianMarriaga <mmmarriaga@unimagdalena.edu.co>
2026-03-25 09:16:50 -0500

    feat: Agregar campos a CourseDtos

--- src/main/java/edu/unimagdalena/lms/api/dto/CourseDtos.java
+++ src/main/java/edu/unimagdalena/lms/api/dto/CourseDtos.java
@@ -1,4 +1,29 @@
 package edu.unimagdalena.lms.api.dto;
+import java.io.Serializable;
+import java.time.Instant;
 
 public class CourseDtos {
+    public record CourseCreateRequest(
+            long instructorId,
+            String title,
+            String status,
+            boolean active
+    ) implements Serializable {}
+
+    public record CourseUpdateRequest(
+            long InstructorId,
+            String title,
+            String status,
+            boolean active
+    ) implements Serializable {}
+
+    public record CourseResponse(
+            long id,
+            String title,
+            String status,
+            boolean active,
+            Instant createdAt,
+            Instant updatedAt,
+            String instructorName
+    ) implements Serializable {}
 }
